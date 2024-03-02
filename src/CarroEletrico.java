ada527bbf5462522f207a2c5e3557274be4673ce
Thiago Figueiró Ribeiro <thiago.figueiro.ribeiro@gmail.com>
2024-03-02 20:38:43 -0300

    Bugfix: Retirada da implementação de SistemaDeTransporte em CarroEletrico. Motivo: A interface já estava implementada na classe mãe (Carro).

--- src/CarroEletrico.java
+++ src/CarroEletrico.java
@@ -1,20 +1,10 @@
-public class CarroEletrico extends Carro implements ModuloEletrico, SistemaDeTransporte {
+public class CarroEletrico extends Carro implements ModuloEletrico {
     // atributos
     // contrutor
     // metodos
 
     public CarroEletrico(String modelo, Integer ano) {
         super(modelo, ano);
-    }
-
-    @Override
-    public void acelerar(int velocidade) {
-        System.out.println("Carro acelerando");
-    }
-
-    @Override
-    public void frear(int velocidade) { // freIo
-        System.out.println("Carro freando");
     }
 
     @Override
@@ -26,14 +16,5 @@ public class CarroEletrico extends Carro implements ModuloEletrico, SistemaDeTra
     public void carregar(int voltagem) {
 
     }
-
-    @Override
-    public void controleDeCarga(double peso, int distancia) {
-
-    }
-
-    @Override
-    public void calcularRota(String[] pontosDeEntrega) {
-
-    }
 }
+
